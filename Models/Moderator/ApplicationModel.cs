18443f390d1cc9b5c1bc0e22955fbf2312b15f8b
Lineblacka <124469910+Lineblacka@users.noreply.github.com>
2023-10-22 18:52:04 +0300

    Design Update
    Views updated

---
+++ Models/Moderator/ApplicationModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ruse2023.Models.Moderator
+{
+    public class ApplicationModel
+    {
+        [Required]
+        [StringLength(500, MinimumLength = 20)]
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ Models/Shopping/ShoppingApplicationModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ruse2023.Models.Shopping
+{
+    public class ShoppingApplicationModel
+    {
+        [Required]
+        public string Description { get; set; } = null!;
+        public byte[] Image { get; set; }
+    }
+}
