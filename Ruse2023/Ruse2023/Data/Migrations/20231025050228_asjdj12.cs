71737b430edfe2b195beb6a1b8505cdb50ba415e
MarioCSharp <mario.18@abv.bg>
2023-10-25 12:15:06 +0300

    updates

---
+++ Ruse2023/Ruse2023/Data/Migrations/20231025050228_asjdj12.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ruse2023.Data.Migrations
+{
+    public partial class asjdj12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Ruse2023/Ruse2023/Models/HomePageModel.cs
@@ -0,0 +1,11 @@
+﻿using Ruse2023.Models.Account;
+using Ruse2023.Models.Store;
+
+namespace Ruse2023.Models
+{
+    public class HomePageModel
+    {
+        public List<StoreApiModel> Products { get; set; }
+        public List<UserDisplayModel> Users { get; set; }
+    }
+}
